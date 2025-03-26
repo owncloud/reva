33019e6b728fa2e7ae893e54e0bd07207e678d64
Michal Klos <michal.klos@kiteworks.com>
2025-03-26 10:16:09 +0100

    feat: user id from federated opaque id

--- pkg/ocm/user/user.go
+++ pkg/ocm/user/user.go
@@ -44,6 +44,31 @@ func RemoteID(id *userpb.UserId) *userpb.UserId {
 	return remoteId
 }
 
+// RemoteIDFromString
+// 1. decode the base64 encoded opaque id from string
+// 2. splitting the opaque id at the last @ to get the opaque id and the domain
+func RemoteIDFromOpaqueID(opaqueID string) *userpb.UserId {
+	remoteId := &userpb.UserId{
+		Type:     userpb.UserType_USER_TYPE_FEDERATED,
+		Idp:      "",
+		OpaqueId: opaqueID,
+	}
+
+	bytes, err := base64.StdEncoding.DecodeString(opaqueID)
+	if err != nil {
+		return remoteId
+	}
+	remote := string(bytes)
+	last := strings.LastIndex(remote, "@")
+	if last == -1 {
+		return remoteId
+	}
+	remoteId.OpaqueId = remote[:last]
+	remoteId.Idp = remote[last+1:]
+
+	return remoteId
+}
+
 // FormatOCMUser formats a user id in the form of <opaque-id>@<idp> used by the OCM API in shareWith, owner and creator fields
 func FormatOCMUser(u *userpb.UserId) string {
 	return fmt.Sprintf("%s@%s", u.OpaqueId, u.Idp)
